16e12cef2f822b88e1d037c3e092ebc9d1bf0f88
Brad Neil <brad@friedorange.xyz>
2023-01-28 18:26:21 +1100

    Remove Video Bold style

--- generate.bat
+++ generate.bat
@@ -5,5 +5,14 @@ fontforge -script source\generate.py
 rem Generate intermediate UFO sources
 for %%f in (source\temp\Quantum*.sfd) do (
 	sfd2ufo %%f source\%%~nf.ufo
-	rem copy features.fea %%~nf.ufo\features.fea
 )
+
+copy features.fea Quantum-Regular.ufo\features.fea
+copy features.fea Quantum-Bold.ufo\features.fea
+copy features.fea QuantumPrint-Regular.ufo\features.fea
+copy features.fea QuantumPrint-Bold..ufo\features.fea
+copy features.fea QuantumRaster-Regular.ufo\features.fea
+copy features.fea QuantumRaster-Bold..ufo\features.fea
+copy features.fea QuantumScreen-Regular.ufo\features.fea
+copy features.fea QuantumScreen-Bold..ufo\features.fea
+copy features.fea QuantumVideo-Regular.ufo\features.fea
