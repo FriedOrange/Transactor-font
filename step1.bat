402f0c8fb2da8ddf54eec2254f96218f7c867e77
Brad Neil <brad@friedorange.xyz>
2023-01-30 22:07:42 +1100

    Draw auxiliary bold glyphs

--- step1.bat
+++ step1.bat
@@ -1,2 +1,3 @@
-fontforge -script source\step1.py normal "source\\glyphs.pbm" "source\\glyphs.csv" "source\\temp\\temp main.sfd"
-fontforge -script source\step1.py halfstep "source\\glyphs halfstep.pbm" "source\\glyphs halfstep.csv" "source\\temp\\temp halfstep.sfd"
+fontforge -script source\step1.py normal "source\\glyphs.pbm" "source\\glyphs.csv" "source\\temp\\step1 main.sfd"
+fontforge -script source\step1.py halfstep "source\\glyphs halfstep.pbm" "source\\glyphs halfstep.csv" "source\\temp\\step1 halfstep.sfd"
+fontforge -script source\step1.py normal "source\\glyphs bold special.pbm" "source\\glyphs bold special.csv" "source\\temp\\step1 bold.sfd"
