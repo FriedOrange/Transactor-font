9e31d74e24ff195f3a3a74278475e46120ee38ad
Brad Neil <brad@friedorange.xyz>
2023-02-03 14:28:48 +1100

    Rename Print #2 variant to Print Mono

--- build.bat
+++ build.bat
@@ -6,5 +6,5 @@ python %USERPROFILE%\AppData\Local\Programs\Python\Python310\Scripts\gftools-bui
 @rem generate proof documents
 @cd fonts\ttf
 set PYTHONUTF8=1
-python %USERPROFILE%\AppData\Local\Programs\Python\Python310\Scripts\gftools-gen-html.py proof -o ..\proof   Quantum-Regular.ttf  QuantumPrint-Regular.ttf  QuantumRaster-Regular.ttf  QuantumScreen-Regular.ttf  QuantumVideo-Regular.ttf  QuantumPrint#2-Regular.ttf  Quantum-Bold.ttf  QuantumPrint-Bold.ttf  QuantumRaster-Bold.ttf  QuantumScreen-Bold.ttf  QuantumPrint#2-Bold.ttf
+python %USERPROFILE%\AppData\Local\Programs\Python\Python310\Scripts\gftools-gen-html.py proof -o ..\proof   Quantum-Regular.ttf  QuantumPrint-Regular.ttf  QuantumRaster-Regular.ttf  QuantumScreen-Regular.ttf  QuantumVideo-Regular.ttf  QuantumPrintMono-Regular.ttf  Quantum-Bold.ttf  QuantumPrint-Bold.ttf  QuantumRaster-Bold.ttf  QuantumScreen-Bold.ttf  QuantumPrintMono-Bold.ttf
 @cd ..\..
